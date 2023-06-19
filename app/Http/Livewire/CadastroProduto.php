04269f4f6547815426c64077ed522a70257676d0
israel silva <Israel524.is@gmail.com>
2023-06-18 21:06:01 -0300

    livewire view ok
    a parte de views e controller do livewire foi feita, mas é necessário re(fazer) o mapeamento de relações do banco de dados

---
+++ app/Http/Livewire/CadastroProduto.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\{Marca, Modelo};
+use Livewire\Component;
+
+class CadastroProduto extends Component
+{
+    public $marca;
+    public $marca_id;
+
+    public $modelos = null;
+    
+    public function mount(Marca $marca){
+        $this->marca = $marca;
+    }
+
+    public function filtroModeloPorMarcaId(){
+        $this->modelos = $this->marca
+                              ->find($this->marca_id)
+                              ->modelo_id;
+                               dd($this->modelos);
+    }
+
+    public function render()
+    {
+        return view('livewire.cadastro-produto');
+    }
+}
