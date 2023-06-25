2b2e1b4444aba1fe162388b960a7985b90cc7b38
israel silva <Israel524.is@gmail.com>
2023-06-25 20:45:06 -0300

    fix de estilos mudança de pasta

--- resources/views/layout/basico.blade.php
+++ resources/views/layout/basico.blade.php
@@ -8,17 +8,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="{{ url('css/estilos.css') }}">
+    <link rel="stylesheet" href="{{ url('storage/modelos/estilos.css') }}">
 </head>
 
-<body class="fundo-dark">
+<body class="bg-secondary">
     <div class="d-flex flex-column wrapper">
         <nav class="navbar navbar-expand-lg bg-dark border-bottom shadow-sm mb-3 ">
             <div class="container ">
                 <div class="row ">
                     <div class="col-auto ">
                         <a href="/">
-                            <img src="{{ asset('/images/logo_1.png') }}" width="150px" height="50px" alt="" />
+                            <img src="{{ url('storage/modelos/logo_1.png') }}" width="150px" height="50px" alt="" />
                         </a>
                     </div>
                 </div>
