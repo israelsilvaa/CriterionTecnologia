8d6de9ef78a881af45b67630028b3a292f68d0c9
israel silva <Israel524.is@gmail.com>
2023-06-25 21:14:41 -0300

    fundo bg-dark do bootstrap

--- resources/views/layout/basico.blade.php
+++ resources/views/layout/basico.blade.php
@@ -8,10 +8,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="{{ url('public/storage/modelos/estilos.css') }}">
+    {{-- <link rel="stylesheet" href="{{ url('public/storage/modelos/estilos.css') }}"> --}}
 </head>
 
-<body class="fundo-dark">
+<body class="bg-dark">
     <div class="d-flex flex-column wrapper">
         <nav class="navbar navbar-expand-lg bg-dark border-bottom shadow-sm mb-3 ">
             <div class="container ">
